91998a1e95cd2f446401f89a21f1b6b13135ec20
VincenzoFerraro0 <ferrarov0@gmail.com>
2025-01-21 22:34:40 +0100

    add: function

--- js/functions.js
+++ js/functions.js
@@ -1,1 +1,22 @@
 
+function generaNumeriCasuali(min, max, ) {
+    const numeriCasuali = [];
+    for (let i = 0; i < 5; i++) {
+        numeriCasuali.push(Math.floor(Math.random() * max)+ min); // Numeri casuali 
+    }
+    return numeriCasuali;
+}
+
+function createCountdown(duration, interval, targetElement) {
+    let counter = duration;
+    const intervalId = setInterval(() => {
+        if (counter == 0) {
+            clearInterval(intervalId);
+            targetElement.innerHTML = 0;
+        } else {
+            targetElement.innerHTML = counter--;
+        }
+    }, interval); 
+
+    return intervalId; 
+}
